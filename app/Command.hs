fb955b734ce0ecfff354cfbd010d43d68168593c
sonatsuer <sonatsuer@gmail.com>
2019-02-19 11:29:10 +0300

    Defined Command and implemented the issue function

--- app/Command.hs
+++ app/Command.hs
@@ -3,11 +3,23 @@ module Command where
 import           Data.Void (Void)
 import qualified Text.Megaparsec as MP
 import qualified Text.Megaparsec.Char as MP
+import           Geometry
 
+data Object
+  = ObjPoint Point
+  | ObjLine Line
+  | ObjCircle Circle
+  deriving Show
 
-data Command = Command
+type Name = String
+
+data Command
+  = Construct Object
+  | Assign Object Name
+  | Discard Name
+  | Show Name
 
 type Parser = MP.Parsec Void String
 
 commandParser :: Parser Command
-commandParser = MP.string "Command" >> MP.eof >> return Command
+commandParser = undefined
--- app/State.hs
+++ app/State.hs
@@ -1,12 +1,48 @@
+{-# LANGUAGE
+    LambdaCase
+#-}
+
 module State where
 
 import           Control.Monad.Trans.State.Strict
 import           Command
+import qualified Data.Map.Strict as M
 
-data GlobalState = GlobalState
+type GlobalState = M.Map Name Object
+type CommandM = State GlobalState
 
 emptyState :: GlobalState
-emptyState = GlobalState
+emptyState = mempty
+
+
+checkName
+  :: Name
+  -> CommandM String
+  -> (Object -> CommandM String)
+  -> CommandM String
+checkName name action1 action2 =
+  (gets $ M.lookup name) >>= \case
+    Nothing ->
+      action1
+    Just obj ->
+      action2 obj
 
 issue :: Command -> State GlobalState String
-issue _ = return "Success"
+issue = \case
+  Construct obj ->
+    return $ show obj
+
+  Assign obj name ->
+    checkName name
+      (modify (M.insert name obj) >> return "Object assigned.")
+      (return . ("Object already exists:\n" ++) . show)
+
+  Discard name ->
+    checkName name
+      (return $ "No object with name " ++ name ++ " exists.")
+      (const $  modify (M.delete name) >> return "Object discarded.")
+
+  Show name ->
+    checkName name
+      (return $ "No object with name " ++ name ++ " exists.")
+      (return . show)

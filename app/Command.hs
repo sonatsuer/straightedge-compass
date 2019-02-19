8f72353d5f52685aac57620a218f78076a2795d6
sonatsuer <sonatsuer@gmail.com>
2019-02-19 18:15:19 +0300

    Made the Construction type a GADT

--- app/Command.hs
+++ app/Command.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE
+    GADTs
+#-}
+
 module Command where
 
 import           Data.Void (Void)
@@ -5,36 +9,35 @@ import qualified Text.Megaparsec as MP
 import qualified Text.Megaparsec.Char as MP
 import           Geometry
 
-data Object
-  = ObjPoint Point
-  | ObjLine Line
-  | ObjCircle Circle
-  deriving Show
-
 type Name = String
 
 data Input a
   = Reference Name
   | Raw a
 
-data Construction
-  = LineLineIntersection (Input Line) (Input Line)
-  | LineCircleIntersection (Input Line) (Input Circle)
-  | CircleCircleIntersection (Input Circle) (Input Circle)
-  | LineThroughPoints (Input Point) (Input Point)
-  | CircleFromPoints (Input Point) (Input Point)
+data Construction a where
+  LineLineIntersection
+    :: Input Line -> Input Line -> Construction (Result Point)
+  LineCircleIntersection
+    :: Input Line -> Input Circle -> Construction (Result Point)
+  CircleCircleIntersection
+    :: Input Circle -> Input Circle -> Construction (Result Point)
+  LineThroughPoints
+    :: Input Point -> Input Point -> Construction (Result Line)
+  CircleFromPoints
+    :: Input Point -> Input Point -> Construction (Result Circle)
 
 data Capture
   = Single String
   | Pair String String
 
-data Command
-  = Construct Construction
-  | Name Construction Capture
-  | Discard Name
-  | Show Name
+data Command a where
+  Construct :: Construction a -> Command a
+  Name :: Construction a -> Capture -> Command a
+  Discard :: Name -> Command a
+  Show :: Name -> Command a
 
 type Parser = MP.Parsec Void String
 
-commandParser :: Parser Command
+commandParser :: Parser (Command a)
 commandParser = undefined
--- app/State.hs
+++ app/State.hs
@@ -1,19 +1,50 @@
 {-# LANGUAGE
     LambdaCase
+  , GADTs
 #-}
 
 module State where
 
 import           Control.Monad.Trans.State.Strict
 import           Control.Monad.Trans.Except
+import           Control.Monad.Trans.Class (lift)
 import           Command
+import           Geometry
 import qualified Data.Map.Strict as M
 
-type GlobalState = M.Map Name Object
+data Object
+  = ObjPoint Point
+  | ObjLine Line
+  | ObjCircle Circle
+  deriving Show
+
+type GlobalState = M.Map String Object
 type CommandM = ExceptT String (State GlobalState)
 
 emptyState :: GlobalState
-emptyState = mempty
+emptyState = M.empty
 
-issue :: Command -> CommandM String
-issue = undefined
+issue :: Command a -> CommandM String
+issue = \case
+  Construct construction ->
+    show . snd <$> evalConstruction construction
+  Name construction capture -> do
+    result <- evalConstruction construction
+    case (capture, result) of
+      (_, _) ->
+        undefined
+  Discard name ->
+    undefined
+  Show name ->
+    undefined
+
+evalConstruction :: Construction a -> CommandM (a, String)
+evalConstruction = \case
+  LineLineIntersection inp1 inp2 -> do
+    l1 <- evalAsLine inp1
+    l2 <- evalAsLine inp2
+    let cons = intersectionOfLines l1 l2
+    return (intersectionOfLines l1 l2, "Constructed:\n" ++ show cons)
+
+evalAsLine :: Input Line -> CommandM Line
+evalAsLine = undefined
